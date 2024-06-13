76ef16ef2315cb1301ba8081e60b9698f7820d48
Mubtasimf443 <mubtasimf443@gmail.com>
2024-06-13 19:51:06 +0600

    Create Footer.jsx

---
+++ Client/src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+import react from 'react';
+
+function Footer(param) {
+  return(
+  <>
+      <footer>
+        <div class="footer-title">
+          Myberr
+        </div>
+        <div class="footer-copyRight-para">
+          All the © copyrights arr reserve in Myberr International 
+        </div>
+        <div class="footer-social-Icon-div">
+          <i class="fa-solid fa-facebook"></i>
+          <i class="fa-solid fa-linkedin" ></i>
+          <i class="fa-solid fa-pinterest"></i>
+          <i class="fa-solid fa-youtube" ></i>
+        </div>
+      </footer>
+  </>
+  );
+}
+
+export default Footer;
